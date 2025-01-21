8a7ac52332cf6636281596db370f0744112bf136
Deepak Mohanty <mohantydeepak284@gmail.com>
2025-01-21 16:46:37 +0530

    edit projectfile

--- src/container/Project/Project.jsx
+++ src/container/Project/Project.jsx
@@ -1,56 +1,65 @@
 import React from "react";
 import "./Project.scss";
-import photo1 from "../../assets/photo1.jpg";
-// import photo2 from "../../assets/photo2.jpg";
-// import photo3 from "../../assets/photo3.jpg";
+import photo1 from "../../assets/A.I.Mock.jpg.png";
+import photo2 from "../../assets/resume.jpg.png";
+import photo3 from "../../assets/chat.jpg.png";
 
 function Projects() {
   const projectData = [
     {
       title: "A.I Mock Interview",
       description:
-        "An online platform where A.I take your interview and give you rewiews.",
+        "An online platform where A.I takes your interview and provides detailed feedback and reviews.",
       image: photo1,
-      link: "https://github.com/deepak-mohanty-web/ai_interview.git",
+      github: "https://github.com/deepak-mohanty-web/ai_interview.git",
     },
     {
-      title: "Resume Bulider",
-      description: "A online platform where you can create your own resume.",
-      image: "path/to/image2.jpg",
-      link: "https://github.com/deepak-mohanty-web/ezy-resume-builder-main.git",
+      title: "Resume Builder",
+      description:
+        "A professional platform where you can create and customize your own resume with various templates.",
+      image: photo2,
+      github:
+        "https://github.com/deepak-mohanty-web/ezy-resume-builder-main.git",
     },
     {
       title: "Chat App",
-      description: "A platform to connect with friends and share moments.",
-      image: "path/to/image3.jpg",
-      link: "https://github.com/deepak-mohanty-web/react-firebase-chat.git",
+      description:
+        "A real-time chat platform to connect with friends and share memorable moments.",
+      image: photo3,
+      github: "https://github.com/deepak-mohanty-web/react-firebase-chat.git",
     },
   ];
 
   return (
-    <section className="projects section" id="projects">
-      <h2 className="section__title">Projects</h2>
-      <span className="section__subtitle">My Work</span>
+    <section className="projects" id="projects">
+      <h2 className="projects__title">Projects</h2>
+      <span className="projects__subtitle">My Work</span>
 
-      <div className="projects__container container">
+      <div className="projects__container">
         {projectData.map((project, index) => (
           <div className="projects__card" key={index}>
-            {/* <img
-              src={project.image}
-              alt={project.title}
-              className="projects__img"
-            /> */}
+            <div className="projects__image-container">
+              <img
+                src={project.image}
+                alt={project.title}
+                className="projects__img"
+              />
+              <div className="projects__overlay">
+                <div className="projects__buttons">
+                  <a
+                    href={project.github}
+                    className="projects__button"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Code
+                  </a>
+                </div>
+              </div>
+            </div>
             <div className="projects__content">
-              <h3 className="projects__title">{project.title}</h3>
+              <h3 className="projects__card-title">{project.title}</h3>
               <p className="projects__description">{project.description}</p>
-              <a
-                href={project.link}
-                className="projects__button"
-                target="_blank"
-                rel="noreferrer"
-              >
-                View Project
-              </a>
             </div>
           </div>
         ))}
